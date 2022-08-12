f2c79256c218d6dd5ab05afbcfbbc57da3a058eb
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-08-12 23:19:08 +0530

    [jayesh] check MaximumNum check

--- GenericAssi/MaximumNumberCheck.cs
+++ GenericAssi/MaximumNumberCheck.cs
@@ -30,5 +30,27 @@ namespace GenericAssi
             }
             throw new Exception("firstNumber,SecondNumber,thirdNumber are same");
         }
+        public static double MaximumdoubleNumber(double firstvalue, double secondvalue, double thirdvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >0 ||
+                firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+                firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >=0)
+            {
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >0 ||
+               secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+               secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >=0)
+            {
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >0 ||
+               thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+               thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >=0)
+            {
+                return thirdvalue;
+            }
+            throw new Exception("firstNumber,SecondNumber,thirdNumber are same");
+        }
     }
 }
--- GenericAssi/Program.cs
+++ GenericAssi/Program.cs
@@ -9,6 +9,8 @@ namespace GenericAssi
             Console.WriteLine("Find Max Number");
             int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 45);
             Console.WriteLine(output);
+            double output1 = MaximumNumberCheck.MaximumdoubleNumber(12.3, 13.5, 45.4);
+            Console.WriteLine(output1);
         }
 
 
