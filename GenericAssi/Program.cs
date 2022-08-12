e665ba163715ad046f932e066f1a63f8a517883e
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-08-12 23:06:14 +0530

    [jayesh] check MaximumNum check

---
+++ GenericAssi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericAssi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Number");
+            int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 45);
+            Console.WriteLine(output);
+        }
+
+
+    }
+}
